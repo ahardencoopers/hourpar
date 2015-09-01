2ef9e3a9f31840338cf1fa8ab3d8668d9d00dd1a
ahardencoopers <ahardencoopers@gmail.com>
2015-09-01 12:33:38 -0400

    Cambiar main para separar salon, rango de horarios y rango de horarios en dias extra

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
-puts "hello world"
+horarios = File.open("test.txt", "r")
+
+horarios.each do |horario|
+  salon = horario.split(",")[0]
+  rangoHorario = horario.split(",")[1]
+  rangoExtra = horario.split(",")[2..-1]
+end
+
+horarios.close
